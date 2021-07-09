adb5a5fa590bbefb5625ce753679b7b8ddab636e
ehab qino <ehabqino@gmail.com>
2021-07-09 18:18:38 +0300

    commit "Create The Course Model"

---
+++ models/course.model.js
@@ -0,0 +1,27 @@
+define(['ojs/ojmodel'], 
+    function() {
+        class CourseModel{
+            constructor(){
+
+            }//end constructor
+            initializeModelCollection(endpoint){
+
+            }//end initializeModelCollection
+            
+            getCoursesList(notify){
+
+            }//end getCoursesList
+            addCourse(id,title,description,notify){
+
+            }//end addCourse
+            updateCourse(id,title,description,notify){
+
+            }//end updateCourse
+            deleteCourse(id,notify){
+
+            }//end deleteCourse
+
+        }//end Class
+        return new CourseModel;
+    
+});
