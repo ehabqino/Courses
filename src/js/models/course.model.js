4f8dc38bb834621da0eee0fcd696f884aeadcc2e
ehab qino <ehabqino@gmail.com>
2021-07-10 08:14:39 +0300

    commit "Initialize the Model and Collection"

--- src/js/models/course.model.js
+++ src/js/models/course.model.js
@@ -1,13 +1,30 @@
+const { oj } = require("@oracle/oraclejet/dist/types");
+
 define(['ojs/ojModel'], 
     function() {
         class CourseModel{
             constructor(){
-
+                this.serverUrl = "http://localhost:2480/";
             }//end constructor
             initializeModelCollection(endpoint){
+                this.courseModelDef = oj.Model.extend({
+                    url : endpoint,
+                    idAttributes : "@rid"
+                });
+                this.courseCollDef = oj.Collection.extend({
+                    url : endpoint,
+                    comparator : "@rid",
+                    model : new this.courseModelDef
+                });
+                this.courses = new this.courseCollDef;
 
             }//end initializeModelCollection
             getCoursesMenu(notify){
+                //api url for all courses
+                let api_url = this.serverUrl + "query/ehabcourses/sql/SELECT FROM course";
+                this.initializeModelCollection(api_url);
+
+               
                 let navData = [
                     { path: '', redirect: 'dashboard' },
                     { path: 'dashboard', detail: { label: 'Dashboard', iconClass: 'oj-ux-ico-bar-chart' } }
