fd0be1a31646806afe18199e8b2105245a0ea83d
ehab qino <ehabqino@gmail.com>
2021-07-12 17:33:57 +0300

    commit "create utils packages"

---
+++ src/js/utils/messageBroker.js
@@ -0,0 +1,17 @@
+define([], 
+    function() {
+        class MsgBroker {
+            constructor(){
+                this.subscribers = {};
+            }//end constructor
+            publish(groupName, data){
+
+            }//end publish
+            subscribe(groupName,callbackNotify){
+
+            }//end subscribe
+
+        }//end Class
+        return new MsgBroker;
+    
+});
