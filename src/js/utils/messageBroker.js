0c3a9ef9bf6e380fd696076bfb3f092bcbde8252
ehab qino <ehabqino@gmail.com>
2021-07-13 07:01:05 +0300

    commit "Implementation of subscribe function"

--- src/js/utils/messageBroker.js
+++ src/js/utils/messageBroker.js
@@ -4,12 +4,27 @@ define([],
             constructor(){
                 this.subscribers = {};
             }//end constructor
-            
+
             publish(groupName, data){
+                if(! Array.isArray(this.subscribers[groupName]))
+                    return;
+                this.subscribers[groupName].forEach(callbackNotify => {
+                    callbackNotify(data);
+                });
 
             }//end publish
 
             subscribe(groupName,callbackNotify){
+                if(! Array.isArray(this.subscribers[groupName]))
+                    this.subscribers[groupName]=[];
+                
+                let exists = false;
+                this.subscribers[groupName].forEach(savedCallback =>{
+                    if(savedCallback.toString() == callbackNotify.toString()) //using toString to compare tw functions
+                        exists=true;
+                });
+                if(!exists)
+                    this.subscribers[groupName].push(callbackNotify);
 
             }//end subscribe
 
